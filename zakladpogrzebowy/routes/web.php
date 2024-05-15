f9a34ffc90a92e93f15ee8860261ea563f606200
SzymonPisula <szymonpisula6@gmail.com>
2024-05-15 21:20:01 +0200

    małe zmiany

--- zakladpogrzebowy/routes/web.php
+++ zakladpogrzebowy/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/subpage1', function () {
+    return view('subpage1');
+});
+
+Route::get('/subpage2', function () {
+    return view('subpage2');
+});
